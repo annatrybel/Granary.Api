f3a43af6fa09ee32f723428f72bd1cca23928d56
Anna Trybel <annakalinovich717@gmail.com>
2026-09-01 16:24:14 +0200

    Add HistoryLog model, service, and controller for audit logging functionality

---
+++ Granary.Api/Services/Interfaces/IHistoryLogService.cs
@@ -0,0 +1,10 @@
+﻿using Granary.Api.Models.Dto;
+using Granary.Api.Services.Results;
+
+namespace Granary.Api.Services.Interfaces
+{
+    public interface IHistoryLogService
+    {
+        Task<ServiceResult<DataTableResponse<HistoryLogDto>>> GetHistoryLogs(DataTableRequest request);
+    }
+}
