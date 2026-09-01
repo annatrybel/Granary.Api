aba04604f88157c566a40262ba835e8c949d84d9
Anna Trybel <annakalinovich717@gmail.com>
2026-09-01 16:31:10 +0200

    Add UserManagementController, service, and error handling for user management functionality

---
+++ Granary.Api/Services/Interfaces/IUserManagementService.cs
@@ -0,0 +1,19 @@
+﻿using Granary.Api.Models.Dto;
+using Granary.Api.Models.Dto.Users;
+using Granary.Api.Services.Results;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Granary.Api.Services.Interfaces
+{
+    public interface IUserManagementService
+    {
+        Task<ServiceResult<DataTableResponse<UserDto>>> GetAllUsers(DataTableRequest request);
+        Task<ServiceResult> LockUser(string userId);
+        Task<ServiceResult> UnlockUser(string userId);
+        Task<ServiceResult<UserDto>> CreateUser(UserDto user, ClaimsPrincipal currentUser);
+        Task<ServiceResult> UpdateUser(UserDto model);
+        Task<ServiceResult<UserDto>> GetUserById(string userId);
+        Task<ServiceResult<List<IdentityRole>>> GetAllRolesAsync();
+    }
+}
